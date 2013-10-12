6be1bb14e486447d0c1f03ee5af03bde0920ed15
Jeff Doppke <jcdoppke@yahoo.com>
2013-10-12 00:07:28 -0400

    Added y grid lines to break down vis

--- app/js/d3-break-down.js
+++ app/js/d3-break-down.js
@@ -1,5 +1,12 @@
 var BreakDown = (function() {
 
+    function _makeXaxis() {
+        return d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(5);
+    }
+
     var divWidth = $('.distro-vis').offsetWidth;
 
     var margin = {top: 7, right: 5, bottom: 20, left: 30};
@@ -29,11 +36,19 @@ var BreakDown = (function() {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    // Add grid lines
+    var yGrid = svg.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(0, " + 0 + ")")
+        .call(_makeXaxis().tickSize(-width, 0, 0).tickFormat(""));
+
+    // Add x-axis
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 
+    // Add y-axis
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis);
@@ -48,8 +63,10 @@ var BreakDown = (function() {
         xAxisSel.call(xAxis);
 
         // Update y-domain and update y-axis
-        y.domain([0, d3.max(newData, function(d) { return d.value + 5; })]);
+        y.domain([0, d3.max(newData, function(d) { return d.value; })]);
         yAxisSel.call(yAxis);
+
+        yGrid.call(_makeXaxis().tickSize(-width, 0, 0).tickFormat(""));
 
         var pointSel = svg.selectAll(".points").data(newData);
 
