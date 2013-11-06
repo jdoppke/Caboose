4a8eac145a95ac8f63bef1f129c81be1561229ad
Jeff Doppke <jcdoppke@yahoo.com>
2013-11-05 22:45:04 -0500

    Fixed quotes

--- app/js/start-up.js
+++ app/js/start-up.js
@@ -1,21 +1,21 @@
 (function() {
 
-    var EVENT_SRC = '';
+    var EVENT_SRC = "";
     // Default sources are just for dev/testing.
-    var DEFAULT_SRC = 'http://radar:8888/node';
-    var DEFAULT_SRC = 'http://192.168.1.84:8888/node';
-    var modal = $('.begin-modal');
-    var overlay = $('.overlay');
-    var startButton = $('#start');
+    var DEFAULT_SRC = "http://radar:8888/node";
+    var DEFAULT_SRC = "http://192.168.1.84:8888/node";
+    var modal = $(".begin-modal");
+    var overlay = $(".overlay");
+    var startButton = $("#start");
     var ES;
 
     var errorStr = "Error connecting, check event source server or resource.";
 
-    startButton.addEventListener('click', startLog, false);
+    startButton.addEventListener("click", startLog, false);
 
     function startLog(e) {
         // Default sources are just for dev/testing.
-        EVENT_SRC = $('input[name="track-path"]').value || DEFAULT_SRC;
+        EVENT_SRC = $("input[name='track-path']").value || DEFAULT_SRC;
         console.log(EVENT_SRC);
         overlay.remove();
         modal.remove();
@@ -24,10 +24,10 @@
 
     function makeConnection() {
         ES = new EventSource(EVENT_SRC);
-        ES.addEventListener('open', connectionSuccess, false);
-        ES.addEventListener('message', Caboose.updateData, false);
-        ES.addEventListener('error', connectionError, false);
-        ES.addEventListener('ping', function() {}, false);
+        ES.addEventListener("open", connectionSuccess, false);
+        ES.addEventListener("message", Caboose.updateData, false);
+        ES.addEventListener("error", connectionError, false);
+        ES.addEventListener("ping", function() {}, false);
     }
 
     function connectionSuccess() {
