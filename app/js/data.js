ceec22f637aa5c99381dd6abd76caeec58e81390
Jeff Doppke <jcdoppke@yahoo.com>
2014-04-05 11:34:05 -0400

    Add data.js

---
+++ app/js/data.js
@@ -0,0 +1,86 @@
+var DATA = (function() {
+
+    var rawData = [];
+
+    //var timelineData = [];
+    var timelineObj = {};
+
+    var rightBound;
+    var leftBound;
+
+    function cleanup() {
+
+        for (var i=0; i<rawData.length; i++) {
+
+            var dateStamp = new Date(rawData[i].date);
+
+            if (dateStamp < leftBound) {
+                rawData.shift();
+            }
+
+        }
+
+    }
+
+    function incrementTimeline() {
+
+        timelineObj = {};
+
+        for (var i=0; i<rawData.length; i++) {
+
+            var dateStamp = Math.floor(+new Date(rawData[i].date) / 1000);
+
+            if (dateStamp in timelineObj) {
+
+                timelineObj[dateStamp].requestCount++;
+
+            } else {
+
+                timelineObj[dateStamp] = {
+                    requestCount: 1,
+                    date: dateStamp
+                };
+
+            }
+
+        }
+
+    }
+
+    function getTimelineData() {
+
+        var timelineData = [];
+
+        for (var time in timelineObj) {
+
+            timelineData.push({
+                date: new Date(+time * 1000),
+                requestCount: timelineObj[time].requestCount
+            });
+
+        }
+
+        return timelineData;
+
+    }
+
+    function compute() {
+
+        rightBound = +new Date();
+        leftBound  = rightBound - (Caboose.conf.timeRange * 60 * 1000);
+
+        // Make sure our data is within the bounds.
+        cleanup();
+
+        // Total up request counts for timeline
+        incrementTimeline();
+
+    }
+
+    return {
+        rawData: rawData,
+        compute: compute,
+        getTimelineData: getTimelineData
+    };
+
+})();
